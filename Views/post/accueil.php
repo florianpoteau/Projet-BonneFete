975fa98268933026e38aa6a0deb2a3af3c406c63
Poteau Florian <florianpoteau59@gmail.com>
2023-06-28 14:54:46 +0200

    add: images

--- Views/post/accueil.php
+++ Views/post/accueil.php
@@ -3,9 +3,45 @@
 <title>Accueil</title>
 </head>
 
-<body>
+<body style="background-color: black;">
 
     <?php require_once 'C:\xampp\htdocs\Projet-BonneFete\Views\navbar.php' ?>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card mt-5">
+                    <div class="card-body">
+                        <h2 class="card-title text-center">Ajouter un post</h2>
+
+
+
+                        <form action="../profil/login" method="post">
+
+
+
+                            <br>
+
+                            <div class="form-group">
+                                <label for="i">Entrez votre texte</label>
+                                <input type="text" class="form-control" id="i" name="i" required>
+                            </div>
+
+                            <br>
+
+
+                            <div class="text-center">
+
+                                <button type="submit" class="btn btn-primary btn-block">Ajouter</button>
+
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
 
