998a6be08559c94a93e8fd9253db642281add23d
Weldarn <marc-antoine.t@hotmail.com>
2023-06-29 11:33:46 +0200

    add: structure profil

--- Views/post/profil.php
+++ Views/post/profil.php
@@ -1,2 +1,36 @@
 <?php require_once 'C:\xampp\htdocs\Projet-BonneFete\Views\navbar.php'; ?>
 <?php require_once 'C:\xampp\htdocs\Projet-BonneFete\Views\head.php'; ?>
+<?php require_once 'C:\xampp\htdocs\Projet-BonneFete\Views\foot.php'; ?>
+
+<body style="background-color: black;">
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card mt-5">
+                    <div class="card-body text-center">
+                        <div class="row align-items-start">
+                            <div class="col-md-6">
+                                <div class="left">
+                                    <img src="" class="rounded-circle mx-auto d-block" alt="Profil" style="width: 50%;">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="right">
+                                    <h2 class="card-title mt-3">Pseudo</h2>
+                                    <p>username</p>
+                                    <h5>Email</h5>
+                                    <p>email@example.com</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body down">
+                            <button type="button" class="btn btn-primary mt-3 mb-2">Changer le mot de passe</button>
+                            <button type="button" class="btn btn-danger mt-3 mb-2">Déconnexion</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
