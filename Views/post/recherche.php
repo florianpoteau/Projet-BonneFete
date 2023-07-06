b7f8518297cb31b0980e37fb93ec400d3f0e6d6f
Weldarn <marc-antoine.t@hotmail.com>
2023-07-06 09:13:06 +0200

    add: Filtrage barre de recherche

--- Views/post/recherche.php
+++ Views/post/recherche.php
@@ -1,25 +1,72 @@
 <script>
-    // Fonctionnement de la barre de recherche
+    // Initialise Isotope container gauche
+    var $grid = $('.wrapper').isotope({
+        itemSelector: '.card',
+        getSortData: {
+            selected: function(itemElem) {
+                var $item = $(itemElem);
+                return ($item.hasClass('selected') ? -1 : 1);
+            }
+        },
+        sortBy: 'selected'
+    });
+    
+    // Initialise Isotope container droite
+    var $gridRight = $('.wrapper-right').isotope({
+    itemSelector: '.card',
+    getSortData: {
+        selected: function(itemElem) {
+            var $item = $(itemElem);
+            return ($item.hasClass('selected') ? -1 : 1);
+        }
+    },
+    sortBy: 'selected'
+});
+
     $(document).ready(function() {
-        $("#pseudoSearch").on("keyup", function() {
-            var value = $(this).val().toLowerCase();
+    $("#pseudoSearch").on("keyup", debounce(function() {
+        var value = $("#pseudoSearch").val();
+    
+        if (value !== undefined && value !== null) {
+            value = value.toLowerCase();
+
+            // Enlevez la classe selected de tous les éléments dans les deux conteneurs
+            $grid.find('.card').removeClass('selected'); 
+            $gridRight.find('.card').removeClass('selected'); 
+
+            // Filtre les éléments de toutes les cartes
             $(".card").filter(function() {
-                var cardTitle = $(this).find('.card-title').text().toLowerCase();
-                var cardText = $(this).find('.card-text').text().toLowerCase();
-                $(this).toggle(cardTitle.includes(value) || cardText.includes(value));
+                var cardTitle = $(this).find(".card-title");
+                var match = false;
+                if (cardTitle.length > 0 && cardTitle.text().trim() !== "") {
+                match = cardTitle.text().toLowerCase().startsWith(value);
+                }
+                $(this).toggle(match);
+
+                if (match) {
+                    $(this).addClass('selected');
+                }
             });
-        });
+
+            // Réorganise de nouveaux l'ordre de tri dans les deux conteneurs
+            $grid.isotope();
+            $gridRight.isotope();
+        }
+        }, 200));
     });
 
-    // $('#profileModal').on('show.bs.modal', function(event) {
-    //     var button = $(event.relatedTarget) // Bouton qui a déclenché la modal
-    //     var username = button.data('username') // Récupère le nom d'utilisateur du data-* attribut
-
-    //     // Récupère les informations de profil
-    //     var profileInfo = getProfileInfo(username);
-
-    //     var modal = $(this)
-    //     modal.find('.modal-title').text('Profil de ' + username)
-    //     modal.find('.modal-body').html(profileInfo)
-    // })
+    // debounce pour éviter un filtrage à chaque milliseconde
+    function debounce(fn, threshold) {
+        var timeout;
+        return function debounced() {
+            if (timeout) {
+                clearTimeout(timeout);
+            }
+            function delayed() {
+                fn();
+                timeout = null;
+            }
+            timeout = setTimeout(delayed, threshold || 100);
+        }
+    }
 </script>
