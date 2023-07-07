0398ecd927512951ff32bad8213c31c4a4382795
Weldarn <marc-antoine.t@hotmail.com>
2023-07-07 13:17:51 +0200

    change: footer

--- Views/foot.php
+++ Views/foot.php
@@ -1,9 +1,9 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
-<footer class="bg-white" style="height: 100px;">
+<footer style="background-color:#5e2d31; height: 100px;">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 d-flex align-items-center justify-content-center" style="margin-top: 3%;">
+        <div class="col-12 text-decoration-none" style="display: flex; align-items: center; justify-content: center;">
                 <a href="../profil/utilisation">Conditions d'utilisation</a>
             </div>
         </div>
